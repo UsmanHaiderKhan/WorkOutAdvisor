f4875bc25ccd3d9439f5913437e9185872f68995
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-15 18:20:42 +0500

    Update Added File on My Git Repository

---
+++ app/src/main/java/com/example/usmankhan/workoutadvisor/WorkAdvisor.java
@@ -0,0 +1,13 @@
+package com.example.usmankhan.workoutadvisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkAdvisor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_advisor);
+    }
+}
